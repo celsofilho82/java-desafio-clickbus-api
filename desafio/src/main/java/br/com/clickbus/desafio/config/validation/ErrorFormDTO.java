d66755d2efd4b9564a66386fcf06cacbe625e4ac
Celso Ribeiro <celsofilho82@gmail.com>
2020-07-12 22:23:34 -0300

    Create a place

---
+++ desafio/src/main/java/br/com/clickbus/desafio/config/validation/ErrorFormDTO.java
@@ -0,0 +1,30 @@
+package br.com.clickbus.desafio.config.validation;
+
+public class ErrorFormDTO {
+
+	private String field;
+	private String error;
+
+	public ErrorFormDTO(String field, String error) {
+		super();
+		this.field = field;
+		this.error = error;
+	}
+
+	public String getField() {
+		return field;
+	}
+
+	public void setField(String field) {
+		this.field = field;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+}
--- desafio/src/main/java/br/com/clickbus/desafio/controller/PlacesController.java
+++ desafio/src/main/java/br/com/clickbus/desafio/controller/PlacesController.java
@@ -1,17 +1,25 @@
 package br.com.clickbus.desafio.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.clickbus.desafio.controller.dto.PlaceDTO;
 import br.com.clickbus.desafio.controller.dto.PlaceDetailsDTO;
+import br.com.clickbus.desafio.controller.form.PlaceForm;
 import br.com.clickbus.desafio.model.Place;
 import br.com.clickbus.desafio.repository.PlaceRepository;
 
@@ -45,4 +53,18 @@ public class PlacesController {
 
 		return ResponseEntity.notFound().build();
 	}
+	
+	@PostMapping
+	@Transactional
+	public ResponseEntity<PlaceDTO> create(@RequestBody @Valid PlaceForm form, UriComponentsBuilder uriBuilder){
+		
+		Place place = form.converter();
+		
+		placeRepository.save(place);
+		
+		URI uri = uriBuilder.path("/places/{id}").buildAndExpand(place.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(new PlaceDTO(place));
+	}
+	
 }
